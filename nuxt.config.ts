db60ba08195513313405fa8eef2d1eada58bb742
zachfullmer <zfullmer9@gmail.com>
2025-10-30 00:51:27 -0600

    add nuxt-svgo dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/test-utils']
-})
+  modules: ["@nuxt/test-utils", "nuxt-svgo"],
+});
