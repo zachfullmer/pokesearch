2ad0087cd63b5ac7f44e27f84a9d9302cfabc137
zachfullmer <zfullmer9@gmail.com>
2025-10-30 15:50:41 -0600

    cache api calls on the server for one hour

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/test-utils", "nuxt-svgo"],
+  routeRules: {
+    "/api/*": { cache: { maxAge: 3600 } },
+  },
 });
